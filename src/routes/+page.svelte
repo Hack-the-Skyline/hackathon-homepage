0562ebe3c7ae790ee62df2d42ae3e337853d7bfc
Ling <39093500+ling-nyc@users.noreply.github.com>
2023-09-01 16:40:31 -0400

    added new skyline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,15 +9,51 @@
     $: dollars = Math.floor(data.balance.balance_cents / 100) + "." + (data.balance.balance_cents % 100).toString().padStart(2, '0');
 </script>
 
+<style>
+    .text-center {
+        font-size: 4rem;
 
-<div class="w-screen h-auto p-5 flex-col"> 
-   
-    <img src={htstext} width="30%" height="30%" class="transition mx-auto invert"/>
+    }
+    .container {
+        position: relative;
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    .image {
+        position: absolute;
+        bottom: 0;
+        width: 50%; /* Adjust the width as needed */
+        height: auto;
+        object-fit: contain;
+    }
+
+    .image.left {
+        left: 0;
+    }
+
+    .image.right {
+        /*transform: scaleX(-1);*/
+        /*supposed to mirror the image but idk how to have it on the right when i do*/
+        transform: translateX(100%);
+    }
+</style>
+
+<div class="w-screen h-auto p-5 flex-col">
+
+    <div>
+        <h1 class="text-center">HACK THE</h1>
+        <h1 class="text-center">SKYLINE</h1>
+    </div>
     <div class="mx-auto flex gap-8 pt-5 justify-center">
         <button class="border-4 rounded-xl text-sky-200 p-2 border-white bg-white font-bold">SIGN UP</button>
         <button class="border-4 rounded-xl text-white p-2 border-white">DONATE</button>
         <button class="border-4 rounded-xl text-white p-2 border-white">ABOUT</button>
     </div>
-    <img src={skyline} width="100%" height="100%" class="transition opacity-80"/>
-    
+    <div class="container">
+        <img src="{skyline}" alt="Skyline" class="image left">
+        <img src="{skyline}" alt="Skyline" class="image right">
+    </div>
+
 </div>
