65ac27052b5a4ca8977b405002988139c49d916b
ling-nyc <lingpeng1209@outlook.com>
2023-09-02 17:53:18 -0400

    animate clouds

---
+++ src/lib/components/clouds.svelte
@@ -0,0 +1,33 @@
+<script>
+    /* To do for this component:
+    - Make the cloud transition from right side of the page back to left seamless
+        - Alternatively limit the cloud to drift back and forth a few pixels in a certain area
+    - Make animating the background work, the bg should float up and down slightly to make the clouds look more alive
+    - Maybe have Kevin draw a middle layer???
+
+    - IMPORTANT Make clouds stop going over the splash text and go underneath it instead, I don't know how to do that.
+ */
+    import CloudBack from '$lib/assets/clouds/cloudback.png';
+    import CloudFront from '$lib/assets/clouds/cloudfront.png';
+    let cloudBackPosition = 0;
+    let cloudFrontPosition = 0;
+
+    function animateClouds() {
+        cloudBackPosition += 10;
+        cloudFrontPosition += 10;
+
+        if (cloudBackPosition >= window.innerWidth || cloudFrontPosition >= window.innerWidth) {
+            cloudBackPosition = 0;
+            cloudFrontPosition = 0;
+        }
+    }
+
+    setInterval(animateClouds, 100);
+</script>
+
+
+<div class="container clouds">
+    <img src={CloudBack} alt="Cloud Back" style="position: absolute; left: {cloudBackPosition}px;" />
+    <img src={CloudFront} alt="Cloud Front" style="position: absolute; left: {cloudFrontPosition}px;" />
+</div>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
     import "../app.css";
     import Navbar from "$lib/components/Navbar.svelte";
     import About from "$lib/components/about.svelte";
+    import Clouds from "$lib/components/clouds.svelte"
     import "@fontsource/passion-one";
 </script>
 
   
   <Navbar/>
+  <Clouds></Clouds>
   <slot/>
-  <About/>>
+  <About/>
